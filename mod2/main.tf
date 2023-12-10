64d999e13d52d7476b767358e08ddef692c191c5
Max Bigras <mbigras22@gmail.com>
2023-12-10 13:19:37 -0800

    Add mod2 that alleviates detail2--progress!

--- mod2/main.tf
+++ mod2/main.tf
@@ -1,5 +1,6 @@
 variable "config" {
   type = object({
+    detail2 = optional(string, "reasonable mod2 default")
     detail3 = string
   })
 }
@@ -8,11 +9,11 @@ output "config" {
   value = var.config
 }
 
-module "mod2" {
-  source = "./mod2"
+module "mod1" {
+  source = "../mod1"
   config = var.config
 }
 
-output "mod2" {
-  value = module.mod2
+output "mod1" {
+  value = module.mod1
 }
